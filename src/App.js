9bb5a4af6043614f93dfe291bb0ef67ae19049a3
JHE H <tetsu@DESKTOP-B3TM6VV.localdomain>
2022-08-28 21:37:59 +0800

    #1 solved by using useRoutes hook

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useRoutes } from "react-router-dom";
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { NavMenu } from './components/NavMenu';
-
-// let RouteList = [{path:'/', component: <Home/>}, {paht:'about', component: <About/>}]
 
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
-    // TODO: find out how to map routes from array
-    // let routes = RouteList.map((r,index) => 
-    // <Route key={index} path={r.path} element={r.component} />)
     return (
       <div className="App">
         <NavMenu/>
       <div className="content">
-      <Routes>
-        <Route path='/' element={<Home/>} />        
-        <Route path='/about' element={<About/>} />
-      </Routes> 
+       <MyRoute/>
       </div>
       </div>
     );
   }
 }
 
+function MyRoute() {
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    { path: "/about", element: <About /> },
+  ]);
+
+  return element;
+}
